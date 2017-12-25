a0bd3394ee8315fb87bf4137587fbcd29b9baf04
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:38:12 +0300

    Create Kisi.cs

---
+++ Kisi.cs
@@ -0,0 +1,19 @@
+kullanarak  System ;
+using  System.Collections.Generic ;
+ System.Linq kullanarak ;
+ System.Text kullanarak ;
+
+isim alanı  Kalitim2
+{
+    sınıf  Kisi
+    {
+        korunan dize  sgk = " 444-55-6666 " ;
+        korunan dize  reklam = " Tekin Ahmet " ;
+
+        kamusal  sanal  boşluk  BilgiAl ()
+        {
+            Console.WriteLine ( " Reklam: {0} " , reklam);
+            Console.WriteLine ( " SGK: {0} " , sgk);
+        }
+    }
+}
