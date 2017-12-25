1c397d7c89095c3d13d30a36610cd284542b6550
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:27:39 +0300

    Create Bitki.cs

---
+++ Bitki.cs
@@ -0,0 +1,26 @@
+kullanarak  System ;
+using  System.Collections.Generic ;
+ System.Linq kullanarak ;
+ System.Text kullanarak ;
+
+ad alanı  BitkiAlemi
+{
+    sınıf  Bitki
+    {
+        genel dizge  Reklam { get ; ayarlayın ; }
+        kamu dizesi  Tur { get ; ayarlayın ; }
+        genel çift  Erkek { get ; ayarlayın ; }
+        genel dizge  Rengi { get ; ayarlayın ; }
+        kamuya açık dize  Mevsim { get ; ayarlayın ; }
+        kamusal int  Yas { get ; ayarlayın ; }
+        kamusal  sanal  boşluk  Buyu ()
+        {
+            Erkek + = 0.5 ;
+            Console.WriteLine ( " Yasasin buyudum boyum " + Boy);
+        }
+        kamusal  sanal  boşluk  FotoSentez ()
+        {
+            Console.WriteLine ( " O2 yi gönderdim hayirli olsun " );
+        }
+    }
+}
