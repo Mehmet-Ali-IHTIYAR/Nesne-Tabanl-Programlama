95200404dbc0d178facce64eece46f4abb4be569
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:33:09 +0300

    Create DonguKiran.cs

---
+++ DonguKiran.cs
@@ -0,0 +1,35 @@
+kullanarak  System ;
+
+ad alanı  DonguKiran
+{
+    birinci sınıf  program
+    {
+        static  void  Ana ( string [] args )
+        {
+            
+            için ( karakter  k = ' A ' , k <= 'Z ' , k ++)
+            {
+                if (k == ' R ' )
+                    mola ;
+                Console.Write (k + "  " " );
+            }
+
+            int  x = 0 , y = 0 ;
+            ise (x < 10 )
+            {
+              Console.WriteLine ( " x {0} öğe y {1} " , x, y);
+                x ++;
+                devam et ;
+                y ++;
+            }
+
+            int  a = 10, b = 20 ;
+            Console.WriteLine ( " {0} + {1} = {2} " , a, b, a + b);
+            Goto  oğlu ;
+
+            Console.WriteLine ( " {0} * {1} = {2} " , a, b, a * b);
+
+            son: Console.WriteLine ( " Program Bitti " );
+        }
+    }
+}
