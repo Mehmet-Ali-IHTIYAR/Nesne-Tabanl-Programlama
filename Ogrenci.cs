bc2352726bb8788bcba6405d1a6055dddeddf1fe
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:40:32 +0300

    Create Ogrenci.cs

---
+++ Ogrenci.cs
@@ -0,0 +1,23 @@
+kullanarak  System ;
+using  System.Collections.Generic ;
+ System.Linq kullanarak ;
+ System.Text kullanarak ;
+
+isim-alanı  Kalitim
+{
+   sınıf  Ogrenci : Insan
+    {
+        kamu  Oğrenci (string reklam): taban (reklam) {}   // Kurucu metodunu temel alıyor
+        kamu  boşluğu  BilgiYaz ()
+        {
+            taban .BilgiYaz (); // Insan clasının BilgiYaz Metodu
+        }
+        kamu  davası  boşu Oku ()
+        {
+            Console.WriteLine ( " Okuyorum abi ... " );
+        }
+         
+      
+
+    }
+}
