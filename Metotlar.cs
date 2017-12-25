dbb0aa962f5e61608b4572eb704849ac887aaca2
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:40:01 +0300

    Create Metotlar.cs

---
+++ Metotlar.cs
@@ -0,0 +1,77 @@
+kullanarak  System ;
+
+ad alanı  MetotlarGece
+{
+    birinci sınıf  program
+    {
+        statik int  f ;  // Global kapsam
+        static  int  alan ( int  a , int  b )
+        {
+            
+            int  d = 0 ;  // Lokal kapsam metodunda geçerli
+            geri a * b,
+        }
+
+        
+        
+        static  void  Yaz ()
+        {
+            Console.WriteLine ( " ************ ALAN HESAPLA ********** " );
+            
+        }
+        static  int  alan ( int  yarıkap )
+        {
+            dönüş Convert.ToInt16 (Math.PI * yarıkap * yarıkap) dönüşü;
+        }
+
+        static  int  alan ( int  k1 , int  k2 , int  k3 )
+        {
+            float  s = (k1 + k2 + k3) / 2 ;
+            return Convert.ToInt16 (Math.Sqrt (s * (s - k1) * (s - k2) * (s - k3)));
+        }
+
+        static  void  Ana ( string [] args )
+        {
+            Yaz ();
+
+            
+
+            Console.WriteLine ( " Bir sekil secmek icin Rakam Giriniz " +
+                " : 1. Daire 2. Dortgen 3. Ucgen " );
+            int  secenek = Convert.ToInt16 (Console.ReadLine ());
+            int  sonuc = 0 ;
+
+            anahtar ( secenek )
+            {
+                davası  1 :
+                    Console.WriteLine ( " Yaricap degerini Giriniz " );
+                    int  yc = Convert.ToInt16 (Console.ReadLine ());
+                    sonucu = alan (yc);
+                    mola ;
+                Durum  2 :
+                    Console.WriteLine ( " Ilk kenar: " );
+                    int  a = Convert.ToInt16 (Console.ReadLine ());
+                    Console.WriteLine ( " Diger Kenar " );
+                    int  b = Convert.ToInt16 (Console.ReadLine ());
+                    sonucu = alan (a, b);
+                    mola ;
+                dava  3 :
+                    Console.WriteLine ( " Ilk kenar: " );
+                    int  x = Convert.ToInt16 (Console.ReadLine ());
+                    Console.WriteLine ( " Ikinci Kenar " );
+                    int  y = Convert.ToInt16 (Console.ReadLine ());
+                    Console.WriteLine ( " Diger Kenar " );
+                    int  z = Convert.ToInt16 (Console.ReadLine ());
+                    sonucu = alan (x, y, z);
+                    mola ;
+                varsayılan :
+                    sonucu = 0 ;
+                    mola ;
+
+
+               }
+            Console.WriteLine ( " Sectiginiz Sekil Icin Hesaplanan Alan: " + sonucu);
+
+        }
+    }
+}
