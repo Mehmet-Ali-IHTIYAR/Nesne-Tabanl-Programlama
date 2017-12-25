cbf82bdf2c47e9d6e59627cc448578224e740c6f
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:17:32 +0300

    Create Agac.cs

---
+++ Agac.cs
@@ -0,0 +1,22 @@
+kullanarak  System ;
+using  System.Collections.Generic ;
+ System.Linq kullanarak ;
+ System.Text kullanarak ;
+
+ad alanı  BitkiAlemi
+{
+    sınıf  Agac : Bitki
+    {
+        kamu  Agac (string reklam)
+        {
+            Reklam = reklam;
+        }
+
+        public  override  void  Buyu ()
+        {
+            taban. Buyu ();
+            Erkek + = 2.5 ;
+           
+        }
+    }
+}
