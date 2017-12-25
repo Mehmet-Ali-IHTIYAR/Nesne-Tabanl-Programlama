9c92824fceaec934a8ee2ece60749286dff8cf43
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:26:17 +0300

    Create BirinciOrnek.cs

---
+++ BirinciOrnek.cs
@@ -0,0 +1,22 @@
+kullanarak  System ;
+
+ad alanı  mehmet {
+
+   sınıf  BirinciOrnek {
+
+   		static  void  Ana ( String [] args )
+   		{
+   			string reklam , soyad ;
+   			ad = " Mehmet " ;
+   			soyad = " İHTİYAR " ;
+
+   			Console.WriteLine (ad + "  " + soyad);
+
+   			Console.ReadKey (); 
+
+   		}
+
+
+   }
+
+}
