9970b6eabb56e16ebbd3102d1c062429824dc349
Mehmet-Ali-IHTIYAR <34844023+Mehmet-Ali-IHTIYAR@users.noreply.github.com>
2017-12-26 01:36:50 +0300

    Create Kalitim.cs

---
+++ Kalitim.cs
@@ -0,0 +1,27 @@
+kullanarak  System ;
+using  System.Collections.Generic ;
+ System.Linq kullanarak ;
+ System.Text kullanarak ;
+
+isim-alanı  Kalitim
+{
+birinci sınıf  program
+    {
+        static  void  Ana ( string [] args )
+        {
+
+            Ogrenci  o1 = yeni  Ogrenci ( " Mehmet Yilmaz " );
+            o1.Yas = 149 ;
+            o1.Ad = " ali " ;
+            o1.BilgiYaz ();
+            o1.Oku ();
+
+            İnsan  i = Yeni  İnsan ( " Bende insanim " );
+            i.BilgiYaz ();
+            
+
+
+
+        }
+    }
+}
